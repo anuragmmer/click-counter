d8634f4fc64514b7603801c8fd527b9eb1edf7e2
anuragmmer <63289577+anuragmmer@users.noreply.github.com>
2023-07-24 14:50:12 +0530

    Create script.js
    This script.js file serves as the foundation of the web page, providing the essential JavaScript code to create an interactive grid of buttons with click counters. The script dynamically generates buttons based on user input, maintaining individual click counts for each button, and implementing functionalities like undoing button clicks and exporting click data as a CSV file.

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let numButtons = parseInt(prompt("Enter the number of buttons (1-20):"));
+    numButtons = Math.max(1, Math.min(20, numButtons));
+
+    const gridContainer = document.getElementById("grid-container");
+    const buttons = []; 
+    const clickCounts = []; 
+
+    let lastClickedButtonIndex = -1; 
+
+    for (let i = 1; i <= numButtons; i++) {
+        const button = document.createElement("button");
+        button.className = "button";
+        button.innerText = `Button ${i} (0)`;
+
+        (function (buttonIndex) {
+            let clickCount = 0;
+            button.addEventListener("click", function () {
+                clickCounts[buttonIndex - 1] = (clickCounts[buttonIndex - 1] || 0) + 1;
+                button.innerText = `Button ${buttonIndex} (${clickCounts[buttonIndex - 1]})`;
+
+                lastClickedButtonIndex = buttonIndex;
+            });
+        })(i);
+
+        gridContainer.appendChild(button);
+        buttons.push(button); 
+        clickCounts.push(0); 
+    }
+
+    const undoButton = document.getElementById("undo-button");
+    undoButton.addEventListener("click", function () {
+        if (lastClickedButtonIndex >= 1 && clickCounts[lastClickedButtonIndex - 1] > 0) {
+            clickCounts[lastClickedButtonIndex - 1]--;
+            buttons[lastClickedButtonIndex - 1].innerText = `Button ${lastClickedButtonIndex} (${clickCounts[lastClickedButtonIndex - 1]})`;
+        }
+    });
+
+    const saveCsvButton = document.getElementById("save-csv-button");
+    saveCsvButton.addEventListener("click", function () {
+        let csvData = "Serial,Clicks\n";
+        buttons.forEach((button, index) => {
+            const serial = index + 1;
+            const clicks = clickCounts[index] || 0;
+            csvData += `${serial},${clicks}\n`;
+        });
+        const csvBlob = new Blob([csvData], { type: "text/csv" });
+        const csvUrl = URL.createObjectURL(csvBlob);
+        const a = document.createElement("a");
+        a.href = csvUrl;
+        a.download = "button_click_data.csv";
+        a.click();
+    });
+});
